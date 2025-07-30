1580040e2a23356a8916f0b4ce3414b53f187340
Walter Waymann <59487134+wwaymann@users.noreply.github.com>
2025-07-30 13:44:45 -0400

    Update server.js

--- server.js
+++ server.js
@@ -23,19 +23,19 @@ app.get('/api/products', (req, res) => {
       _id: "1",
       nombre: "Maceta DOBO 1",
       precio: "$5.000",
-      imagen: "https://via.placeholder.com/300x300?text=Maceta+1"
+      imagen: "https://static.wixstatic.com/media/a36313_7412457b195f4a7689ccfe42d1308137~mv2.jpg"
     },
     {
       _id: "2",
       nombre: "Maceta DOBO 2",
       precio: "$6.500",
-      imagen: "https://via.placeholder.com/300x300?text=Maceta+2"
+      imagen: "https://static.wixstatic.com/media/a36313_a724e9942ef9448094c2a8019ac55220~mv2.png"
     },
     {
       _id: "3",
       nombre: "Edición Especial",
       precio: "$7.200",
-      imagen: "https://via.placeholder.com/300x300?text=Maceta+3"
+      imagen: "https://static.wixstatic.com/media/a36313_6b0af97dcc4e4128bc5e4cbc59cbb0b0~mv2.jpg"
     }
   ];
 
